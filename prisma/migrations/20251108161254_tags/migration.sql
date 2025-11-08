92551d05ef27807b2543ce48207fe933207e3d6f
Basil <okwutebasil01@gamil.com>
2025-11-08 17:13:08 +0100

    tags

---
+++ prisma/migrations/20251108161254_tags/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - The values [BOTH] on the enum `UserRole` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "UserRole_new" AS ENUM ('HOST', 'ATTENDEE');
+ALTER TABLE "public"."users" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "users" ALTER COLUMN "role" TYPE "UserRole_new" USING ("role"::text::"UserRole_new");
+ALTER TYPE "UserRole" RENAME TO "UserRole_old";
+ALTER TYPE "UserRole_new" RENAME TO "UserRole";
+DROP TYPE "public"."UserRole_old";
+ALTER TABLE "users" ALTER COLUMN "role" SET DEFAULT 'ATTENDEE';
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "events" ALTER COLUMN "tags" DROP NOT NULL,
+ALTER COLUMN "tags" SET DATA TYPE TEXT;
